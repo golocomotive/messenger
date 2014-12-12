795fbd0926e4d9145f6c253a2f6bfc81ec45c30b
Martin Streicher <martin.streicher@gmail.com>
2014-12-11 22:05:00 -0500

    Conform to style.

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_many :groups, through: :memberships, class_name: 'Messenger::Group'
   has_many :memberships, class_name: 'Messenger::Membership', as: :member
-  has_many :receipts, class_name: 'Messenger::Receipt', as: :recipient
   has_many :messages, class_name: 'Messenger::Message', through: :receipts
+  has_many :receipts, class_name: 'Messenger::Receipt', as: :recipient
 
   validates :name, presence: true, uniqueness: true
 end
